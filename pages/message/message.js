c8fc03b0aa178c80fc481a1866dc50d767f6ae1e
hitstu0 <2064355949@qq.com>
2023-11-21 19:28:33 +0800

    未登录页面

--- pages/message/message.js
+++ pages/message/message.js
@@ -2,8 +2,29 @@ Page({
   /**
    * 生命周期函数--监听页面加载(获取用户的微信信息)
    */
-  onLoad: function () {
+  data: {
+    login: false
   },
+  onLoad: function () {
+    try{
+      var session = wx.getStorageSync('user')
+      if(session == null || session == 'undefined') {
+        //未登录
+        this.login = false
+      } else {
+        //登录
+        this.login = true;
+      }
+    }catch (e) {
+      console.log('读取session发生错误')
+    }
+  },
+
+  onLoginButtonClick: function () {
+    wx.navigateTo({
+      url: '/pages/second/second'
+    });
+  }
 
   /**
    * 生命周期函数--监听页面初次渲染完成（以用户微信头像为条件获取用户的姓名）
