b1c97edbe28a682df6886b9ce49ce5663c501693
250HandsomeLiang <2502481961@qq.com>
2023-11-24 18:56:17 +0800

    完成了个人中心的简历上传和简历查看

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,31 +1,215 @@
 // pages/mine/mine.js
+const db=wx.cloud.database()
+const _=db.command
 Page({
     data: {
-  
+      login:true,
+      //已经沟通
+      communication:0,
+      //已经投递
+      deliver:0,
+      headUrl:null,
+      name:"用户名",
+      userId:null,
+      pdfFile:null
     },
-    //个人中心的消息,点击跳转到我的消息
-    info(){
+    /**
+     * 点击进入登入页面
+     * @param {*} e 
+     */
+    onLoginButtonClick(e){
       wx.navigateTo({
-        url: '../info/info',
+        url: '../login/login',
       })
     },
-    //个人中心的实名认证,点击跳转到实名认证
-    realName() {
-      wx.navigateTo({
-        url: '../realName/realName',
-      })
+      /**
+   * 生命周期函数--监听页面加载
+   */
+    onLoad(options) {
+      this.checkLogin()
+    },
+    /**
+     * 页面显示时才加载
+     */
+    onShow(){
+      this.getUser()
+    },
+    /**
+     * 加载用户信息
+     */
+    getUser(){
+      var id=wx.getStorageSync("userId")
+      let that=this
+      that.data.userId=id
+      db.collection("user").doc(id).get()
+      .then((result) => {
+        console.log("个人用户信息:",result)
+        that.data.userData=result.data
+        that.setData({
+          headUrl:result.data.headUrl,
+          name:result.data.name,
+          deliver:result.data.deliver,
+          communication:result.data.communication,
+          pdfFile:(result.data.resume.length==0||result.data.resume==null)?null:result.data.resume
+        })
+      }).catch((err) => {
+        console.log("加载信息失败",err)
+      });
+    },
+
+    /**
+     * 判断是否登入成功
+     */
+    checkLogin(){
+      //判断是否登入成功
+      var status = wx.getStorageSync('status')    
+      try{
+        if(status == 0 || status == null) {        
+          //未登录
+          this.setData({
+            login: false,
+          });
+        } else {
+         //登录
+          this.setData({
+            login: true,
+          });
+        }
+      }catch (e) {
+        console.log('读取session发生错误' + e)
+      }
+    },
+    /**
+     * 检查文件是否为PDF
+     */
+    isValidPDF(filePath){    
+        console.log(filePath)    
+        // 获取文件后缀名
+        const fileExtension = filePath.split('.').pop().toLowerCase();
+
+        // 判断文件是否为 PDF
+        if (fileExtension === 'pdf') {
+          return true
+        } else {
+          return false
+        }
+    },
+    //查看简历
+    showPdf(e){
+      if(this.data.pdfFile!=null){
+        wx.cloud.downloadFile({
+          fileID: this.data.pdfFile,
+          success: res => {
+            // 2. 下载成功后，可以通过 res.tempFilePath 获取文件的临时路径
+            const tempFilePath = res.tempFilePath;
+        
+            // 3. 使用 wx.saveFile 将文件保存到本地
+            wx.saveFile({
+              tempFilePath: tempFilePath,
+              success: saveRes => {
+                // 4. 文件保存成功后，可以通过 saveRes.savedFilePath 获取保存在本地的文件路径
+                const savedFilePath = saveRes.savedFilePath;
+        
+                // 5. 使用 wx.openDocument 打开保存在本地的文件
+                wx.openDocument({
+                  filePath: savedFilePath,
+                  success: openRes => {
+                    console.log('打开文档成功');
+                  },
+                  fail: openErr => {
+                    console.error('打开文档失败', openErr);
+                  }
+                });
+              },
+              fail: saveErr => {
+                console.error('保存文件失败', saveErr);
+              }
+            });
+          },
+          fail: err => {
+            console.error('下载文件失败', err);
+          }
+        });
+      }else{
+        wx.showModal({
+          title: '提示',
+          content: '请先上传PDF简历',
+        })
+      }
+
+    },
+    //提交简历附件
+    submitResume() {
+      console.log("上传简历附件只能是PDF")
+      // 在小程序中选择文件
+      let that=this
+      wx.chooseMessageFile({
+        count: 1,
+        type: 'file',
+        success(res) {
+          var tempFilePath=res.tempFiles[0].path
+          console.log(res)
+          if(that.isValidPDF(tempFilePath)){
+            wx.showLoading({
+              title: '上传中...',
+            });
+            // 上传文件到服务器
+            wx.cloud.uploadFile({
+              cloudPath: "resume/" + new Date().getTime() + '.pdf',
+              filePath: tempFilePath,
+              success(uploadRes) {
+                console.log('上传成功', uploadRes);
+                // 在这里可以处理上传成功后的逻辑
+                var filePath=uploadRes.fileID
+                that.setData({
+                  pdfFile:filePath
+                })
+                //将数据更新到user表中
+                db.collection("user").doc(that.data.userId).update(
+                  { data:{
+                    resume:filePath
+                    }
+                  }
+                ).then((dataRes)=>{
+                  console.log("更新user表简历成功")
+                  // 隐藏加载提示
+                  wx.hideLoading();
+                }).catch(dataErr=>{
+                  console.log("更新user表失败")
+                  wx.hideLoading();
+                })
+
+              },
+              fail(error) {
+                console.error('上传失败', error);
+                // 处理上传失败的逻辑
+                // 隐藏加载提示
+                wx.hideLoading();
+              },
+            });
+          }else{
+            wx.showModal({
+              title: '提示',
+              content: '请上传PDF简历',
+            })
+          }
+
+        },
+      });
     },
     //个人中心的我的余额,点击跳转到我的余额
-    restMoney() {
+    resumeChange() {
+      console.log("编辑在线简历")
       wx.navigateTo({
-        url: '../restMoney/restMoney',
+        url: './resume/index',
       })
     },
     //个人中心的意见反馈,点击跳转到意见反馈
     view() {
-      wx.navigateTo({
-        url: '../view/view',
-      })
+      console.log("意见反馈")
+      // wx.navigateTo({
+      //   url: '../view/view',
+      // })
     },
     //我的地址
     addr(){
