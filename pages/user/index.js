29a6b5bbd747598b71e29a675a3a492c27df24cd
250HandsomeLiang <2502481961@qq.com>
2023-11-24 15:28:18 +0800

    更新了个人用户页面的模板

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,103 +1,42 @@
-// import {
-//     request
-// } from "../../requests/index.js";
-// Pages/user/index.js
-var app=getApp()
+// pages/mine/mine.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-        userInfo: {},
-        sendJobsNums: "99+",
-        collectNums: 0,
-        expectCities: 0,
-        expectJobs: 0,
-        // 用户登录凭证
-        loginCode: {}
+  
     },
-    // code和iv可解密用户信息encryptedData
-    LoginInfo: {
-        code: '',
-        encryptedData: '',
-        iv: ''
+    //个人中心的消息,点击跳转到我的消息
+    info(){
+      wx.navigateTo({
+        url: '../info/info',
+      })
     },
-    Authorization: {
-        token: 'asda'
+    //个人中心的实名认证,点击跳转到实名认证
+    realName() {
+      wx.navigateTo({
+        url: '../realName/realName',
+      })
     },
-    
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-        // 1.获取缓存数据，初始化页面,包括userInfo，hasSendJobs，collect，expectCities，expectJobs
-        const userInfo = wx.getStorageSync("userInfo");
-        const collect = wx.getStorageSync("collects") || [];
-        const expectCities = wx.getStorageSync("expectCities") || [];
-        const expectJobs = wx.getStorageSync("expectJobs") || [];
-        this.setData({
-            userInfo,
-            collectNums: collect.length,
-            expectCities: expectCities.length,
-            expectJobs: expectJobs.length
-        })
-        // 2.wx.login获取code
-        var that = this;
-        wx.login({ //login流程
-            success: function (res) { //登录成功
-                console.log(res);
-                if (res.code) {
-                    console.log("成功==>wx.login==>获取登录凭证code")
-                    that.setData({
-                        loginCode: res.code
-                    })
-                } else {
-                    console.log('失败==>wx.login==>获取登录凭证code' + res.errMsg)
-                }
-            }
-        });
-        this.Authorization.token = app.globalData.token
+    //个人中心的我的余额,点击跳转到我的余额
+    restMoney() {
+      wx.navigateTo({
+        url: '../restMoney/restMoney',
+      })
     },
-    // 1.点击登录
-    getUserProfile(e) {
-        console.log("获取用户信息")
+    //个人中心的意见反馈,点击跳转到意见反馈
+    view() {
+      wx.navigateTo({
+        url: '../view/view',
+      })
     },
-    // 3.设置resumeUrl
-    async setUserResume() {
-        const resumeUrl=this.data.userInfo.resume
-        this.setData({
-            resumeUrl,
-            resumeTime: resumeUrl.substring(resumeUrl.lastIndexOf('_') + 1, resumeUrl.lastIndexOf('-')),
-            resumeName: resumeUrl.substring(resumeUrl.lastIndexOf('-') + 1)
-        })
-        wx.setStorageSync('resumeUrl', resumeUrl)
+    //我的地址
+    addr(){
+      wx.navigateTo({
+        url: '../myAddr/myAddr',
+      })
     },
-    // 2.根据code和iv解密用户信息encryptedData
-    async getMegFromServer() {
-        console.log("loginInof==>")
-        console.log(this.LoginInfo)
-        var that=this
-        const result = await request({
-            url: "/user/wx/handleWXMsg",
-            data: this.LoginInfo,
-            method: 'POST',
-            header: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                "token":that.Authorization.token
-            }
-        });
-        console.log("handleWXMsg==>结果")
-        console.log(result)
-        this.setData({
-            userInfo: result.data
-        })
-
-        // 设置用户简历
-        this.setUserResume(this.data.userInfo.myEncrypt)
-        // 将用户信息放入缓存
-        wx.setStorageSync("userInfo", this.data.userInfo);
-
-        app.globalData.uri= app.globalData.uri+this.data.userInfo.openId
+    //退出登录
+    signOut(){
+      wx.navigateTo({
+        url: '../logs/log',
+      })
     }
-})
+  })
