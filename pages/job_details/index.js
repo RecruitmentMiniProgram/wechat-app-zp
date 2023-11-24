51c8c57d0c36c825cebe5453dff299405f6c563f
hitstu0 <2064355949@qq.com>
2023-11-24 16:01:00 +0800

    投递简历

--- pages/job_details/index.js
+++ pages/job_details/index.js
@@ -1,3 +1,15 @@
+function MsgInfo(role, msg, time) {
+  this.role = parseInt(role, 10);
+  this.msg = msg;
+  this.time = parseInt(time, 10);
+}
+
+function ChatInfo(id, type, time) {
+  this.id = id
+  this.type = parseInt(type, 10)
+  this.time = parseInt(time, 10)
+}
+
 import { request } from "../../requests/index.js";
 const httputil = require("../../utils/httputil.js") //一定要引入，根据你自己写的上传文件路径
 var out_photo = "";
@@ -33,11 +45,21 @@ Page({
     // 工作信息全局对象
     jobInfoStorage: {},
     onLoad: function (options) {
+      var status = wx.getStorageSync('status')
+      status = 1
+      var userId = wx.getStorageSync('userId')
+      userId = 'a72823ff655c97f60024736e3cc48fed'
+      this.setData({
+        status: status,
+        userId: userId
+      })
+
       // console.log(options)
       this.setData({
-          // jobId: options.jobId,
+          jobId: options.jobId,
           // jobType:options.jobType
       });
+      this.setData({jobId: "a72823ff655d6d7f0034c3f07e2d18c8"})
       this.getJobDetail(this.data.jobId);
       // this.incrementScore(this.data.jobId);
       // this.getSimilarJobDetail(this.data.jobId,this.data.jobType);
@@ -76,154 +98,27 @@ Page({
             })
         }
     },
-    // 预览简历
-    openFile() {
-        wx.downloadFile({
-            url: this.data.resumeUrl, //要预览的 PDF 的地址
-            success: function (res) {
-                console.log(res);
-                if (res.statusCode === 200) { //成功
-                    var Path = res.tempFilePath //返回的文件临时地址，用于后面打开本地预览所用
-                    wx.openDocument({
-                        fileType: 'pdf', // 需要写上文件类型才能预览，不让回找系统应用打开，体验不好
-                        filePath: Path, //要打开的文件路径
-                        success: function (res) {
-                            console.log('打开 PDF 成功');
-                        }
-                    })
-                }
-            },
-            fail: function (res) {
-                console.log(res); //失败
-            }
-        })
-    },
-    // 点击立即投递
-    resumeSubmit(e) {
-        // 判断是否已登录
-        const userInfo = wx.getStorageSync('userInfo')
-        if (!userInfo) {
-            wx.showToast({
-                title: '请先登录',
-                icon: 'none',
-                image: '',
-                duration: 1000,
-                mask: false,
-            });
-            return;
-        } else {
-            this.setData({
-                isLogin: true
-            })
-        }
-        // 判断是否已投递
-        if (this.data.submit_stat == "已投递") {
-            wx.showToast({
-                title: '您已投递',
-                icon: 'none',
-                image: '',
-                duration: 1000,
-                mask: false,
-            });
-            return
-        }
-        // 判断缓存是否有简历
-        const resumeUrl = wx.getStorageSync('resumeUrl')
-        if (resumeUrl) {
-            this.setData({
-                resumeUrl,
-                resumeTime: resumeUrl.substring(resumeUrl.lastIndexOf('_') + 1, resumeUrl.lastIndexOf('-')),
-                resumeName: resumeUrl.substring(resumeUrl.lastIndexOf('-') + 1)
-            })
-        }
-        this.clickPup()
+  
 
-    },
-    // 提交投递请求
-    async sendResume() {
-        const userInfo = wx.getStorageSync('userInfo')
-        this.Userdeliver.fromUserId = userInfo.id;
-        this.Userdeliver.toHrId = this.data.jobObj.jobAuthorId;
-        this.Userdeliver.resumeUrl = userInfo.resume;
-        this.Userdeliver.jobId = this.data.jobObj.jobId;
-        const result = await request({ url: "/own/user/saveDeliver", data: this.Userdeliver });        
-        console.log("发送给hr后返回的结果为：")
-        console.log(result)
-        this.setData({
-            submit_stat: "已投递"
-        })
-        // 发送已投递信息给hr
-        this.sendMessage(userInfo.openId,this.data.jobObj.jobAuthorId, this.data.jobObj.jobName)
-        // 关闭弹出窗
-        this.clickPup()
-        wx.showToast({
-            title: '投递成功',
-            icon: 'none',
-            image: '',
-            duration: 1000,
-            mask: false,
-        });
-        this.handleSendResumeAdd()
-    },
-    // 发送消息
-    async sendMessage(fromOpenId, toOpenId, news) {
-        // const result = await request({ url: "/upload/saveMessage", data: {fromOpenId,toOpenId,news} });
-        // const result = await request({ url: "/own/user/saveMessage2", data: {fromOpenId,toOpenId,news},method: 'POST', header: { "Content-Type": "application/json" } });
-        console.log("发送消息了")
-        console.log(result)
-    },
+   
     // 获取职位详情数据
     async getJobDetail(jobId) {
       var that = this;
-      // console.log(jobId);
-
       db.collection('post').doc(jobId).get({
         success:function(res){
           that.jobInfoStorage = res.data;
           const company_id = res.data.companyId;
-          const companyPromise = db.collection('company').where({companyId:company_id}).get();
+          const companyPromise = db.collection('company').doc(company_id).get();
           companyPromise.then(companyRes=>{
-            res.data.company = companyRes.data[0]; 
-            // 判断是否都投递过该职位
-            that.hasSendJob();
-            // 1 获取缓存中的商品收藏的数组
-            let collects = wx.getStorageSync("collects") || [];
-            // 判断当前商品是否被收藏
-            let isCollect = collects.some(v => v.jobId === that.jobInfoStorage.jobId);
+            res.data.company = companyRes.data; 
             that.setData({
                 jobObj: res.data,
-                isCollect
             });
-            // console.log(res.data);
           })
         }
       });
     },
-    // 判断该职位是否在缓存数组中
-    hasSendJob() {
-        // 1 获取缓存中的发送简历的数组
-        let hasSendJobs = wx.getStorageSync("hasSendJobs") || [];
-        // 判断当前职位是否已发送过
-        // console.log("w",hasSendJobs)
-        console.log(this.jobInfoStorage.jobId)
-        let isSubmit = hasSendJobs.some(v => v.jobId === this.jobInfoStorage.jobId);
-        console.log("是否已投递")
-        console.log(isSubmit)
-        if (isSubmit) {
-            this.setData({
-                submit_stat: '已投递'
-            })
-        }
-    },
-    // 获取相似职位详情数据
-    // async getSimilarJobDetail(jobId) {
-    //     const { isSimilar } = "1";
-    //     const result = await request({ url: "/home/jobdata", data: { jobId, isSimilar } });
-    //     this.setData({
-    //         similarJobs: result.list
-    //     })
-    // },
-    // 点击轮播图 放大预览
+
     handlePreviewImage(e) {
         // 1 先构造要预览的图片数组
         const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
@@ -239,60 +134,113 @@ Page({
         });
 
     },
-    // 点击收藏图标
-    handleCollectAdd() {
-        let isCollect = false;
-        // 1 先获取缓存中的购物车数组
-        let collects = wx.getStorageSync("collects") || [];
-        // 2 判断商品对象是否存在于购物车的数组中
-        let index = collects.findIndex(v => v.jobId === this.jobInfoStorage.jobId);
-        if (index != -1) {
-            // 能找到 已经收藏过 再点击则取消收藏
-            collects.splice(index, 1);
-            isCollect = false;
-            wx.showToast({
-                title: '取消成功',
-                icon: 'success',
-                mask: true
-            });
+
+    async updateChatList(id, _id) {
+      //读取后插入新消息列表
+      var chatListResult = await db.collection('chat_list').where({user_id: id}).get()
+      if(chatListResult.data.length == 0) return
+      var dbList = chatListResult.data[0].data
+      var index = -1
+      for(let i = 0; i < dbList.length; ++ i) {
+        if(dbList[i].id == _id) {
+          index = i
+          break
+        }
+      }
+      if(index != -1) dbList.splice(index, 1)
+      var chatInfo = new ChatInfo(_id, 1, (Date.now()/1000))
+      dbList.unshift(chatInfo)
+
+      await db.collection('chat_list').where({user_id: id}).update({
+        data:{
+          data: dbList
+        }
+      })
+ },
+
+    //企业登录不显示下栏
+    //用户未登录，不显示聊天按钮，按钮提示登录
+    async submitResume() {
+        var userId = this.data.userId
+        var status = this.data.status
+        if(status == 0) {
+          //跳转到登录页面
+          wx.navigateTo({
+            url: '/pages/login/login'
+          });
         } else {
-            // 没有收藏过
-            collects.push(this.jobInfoStorage);
-            isCollect = true;
-            wx.showToast({
-                title: '收藏成功',
-                icon: 'success',
-                mask: true
+          //判断是否有消息，没有则创建
+          var id = await this.judgeOrEditChatHistory(userId, this.data.jobId)
+          //有则发送投递简历消息
+          var msg ="您好，我非常希望能够得到这个岗位的面试机会，如果岗位还有空缺，希望能得到机会，感谢。"
+          //写消息时将消息栏自动置顶到对应用户列表中
+          var msgInfo = new MsgInfo(1, msg, (Date.now()/1000))
+          //更新数据库
+          const db = wx.cloud.database()
+          const _ = db.command
+          // 更新聊天数据
+          await db.collection('chat_history').doc(id).update({
+                data:{
+                  data: _.push(msgInfo),
+                  enter_red: true
+                }
+          })
+
+            //更新对方消息栏
+            var uid = this.data.jobObj.companyId
+            this.updateChatList(uid, id)
+            //更新自己的消息栏
+            var uuid = this.data.userId
+            this.updateChatList(uuid, id)
+
+            wx.navigateTo({
+              url: '/pages/chat/chat?type=' + 1 + '&id=' + id,
             });
         }
-        // 5 把收藏夹重新添加回缓存中
-        wx.setStorageSync("collects", collects);
-        this.setData({
-            isCollect
-        })
     },
-    // 将已投递简历存到缓存中
-    handleSendResumeAdd() {
-        // 1 先获取缓存中已投递职位数组
-        let hasSendJobs = wx.getStorageSync("hasSendJobs") || [];
-        hasSendJobs.push(this.data.jobObj);
-        // 5 把收藏夹重新添加回缓存中
-        wx.setStorageSync("hasSendJobs", hasSendJobs);
+    
+    async judgeOrEditChatHistory(userId, postId) {
+      const db = wx.cloud.database()
+      let userResult = await db.collection('user').doc(userId).get()
+      var userName = userResult.data.name
+      let chatResult = await db.collection('chat_history').where({user_id: userId, post_id: postId}).get()
+      var id = null
+      if(chatResult.data.length == 0) {
+        var job = this.data.jobObj
+        //如果没有则创建聊天项，初始化消息记录,再进入chat页面
+        var res = await  db.collection('chat_history').add({
+          // data 字段表示需新增的 JSON 数据
+          data: {
+            company_id: job.companyId,
+            enter_name:job.company.name,
+            enter_red: false,
+            post_id: job._id,
+            post_name: job.name,
+            user_id: userId,
+            user_name: userName,
+            user_red: false,
+            data:[{
+              msg:"岗位介绍",
+              role: 3,
+              time: (Date.now()/1000)
+            }]
+          },
+        })
+
+        id = res._id
+      } else {
+        id = chatResult.data[0]._id
+      }
+      return id
+    },
+    async goToChatPage(e) {
+      var userId = this.data.userId
+      //查看是否有聊天项
+      var id = await this.judgeOrEditChatHistory(userId, this.data.jobId)
+      //如果有则直接跳转chat页面
+      
+      wx.navigateTo({
+      url: '/pages/chat/chat?type=' + 1 + '&id=' + id,
+    });
     }
 })
-// 表单提交，上传文件
-// function formSubmit() {
-//     var photo = [out_photo];
-//     httputil.send_photo(photo, function (res) {
-//         //成功回调函数  你随便做操作
-//         console.log("图片上传成功");
-
-//         wx.showToast({
-//             title: '投递成功',
-//             icon: 'none',
-//             image: '',
-//             duration: 1000,
-//             mask: false,
-//         });
-//     })
-// }
