c3d6a853438bce3990b694e05fae4465da815f18
Wayne Zheng <zhaowen850@gmail.com>
2023-11-25 17:00:55 +0800

    finished search function(only for position) and improved the UI of jobList

--- cloudfunctions/searchPost/index.js
+++ cloudfunctions/searchPost/index.js
@@ -1,27 +1,43 @@
 const cloud = require('wx-server-sdk');
-
-cloud.init();
-
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV });
 const db = cloud.database();
 
 exports.main = async (event, context) => {
   const { keyword } = event;
+  // try {
+  //   const collectionInfo = await db.collection('post').limit(1).get();
+  //   const fields = collectionInfo.data[0];
+  //   const result = await db.collection('post')
+  //     .where(
+  //       db.command.or(
+  //         // 对集合中所有字段应用模糊搜索
+  //         Object.keys(fields).map(field => ({
+  //           [field]: db.RegExp({ regexp: keyword, options: '' })
+  //         }))
+  //       )
+  //     )
+  //     .get();
+
+  //   return result;
+  // } catch (err) {
+  //   console.error(err);
+  //   return err;
+  // }
 
   try {
-    const collectionInfo = await db.collection('yourCollection').limit(1).get();
-    const fields = collectionInfo.data[0];
-    const result = await db.collection('post')
-      .where(
-        db.command.or(
-          // 对集合中所有字段应用模糊搜索
-          Object.keys(fields).map(field => ({
-            [field]: db.RegExp({ regexp: keyword, options: 'i' })
-          }))
-        )
-      )
+    var result = await db.collection('post')
+      .where({position: keyword})
       .get();
 
-    return result.data;
+    return result;
+    // const wxContext = cloud.getWXContext()
+
+    // return await {
+    //   msg:"搜索地点",
+    //   data:result,
+    //   openid: wxContext.OPENID,
+    //   appid: wxContext.APPID
+    // }
   } catch (err) {
     console.error(err);
     return err;
