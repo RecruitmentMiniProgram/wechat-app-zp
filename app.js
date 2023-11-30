e2133fc3cc49b71a02fcceba0bbbdb788d434c89
hitstu0 <2064355949@qq.com>
2023-11-30 10:38:14 +0800

    info

--- app.js
+++ app.js
@@ -20,9 +20,13 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        env: 'cloud1-1gqwq0cu50854c5d',
+        //我们的云环境
+        // env: 'cloud1-1gqwq0cu50854c5d',
+        //甲方的云环境
+        env:'haianjiuye-9gwh0gp7bb2e3aa1',
         traceUser: true,
-      })
+    })
+
     }
 
     this.globalData = {}
