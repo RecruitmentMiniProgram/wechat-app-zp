fe6d25dc82c6237b8c36aa9b9106343eab05ca09
Wayne Zheng <zhaowen850@gmail.com>
2023-12-01 21:19:23 +0800

    添加了职位分类的组件category；改了职位分类的json；兼职排序；搜索问题仍未解决

--- pages/category_job/index.js
+++ pages/category_job/index.js
@@ -9,26 +9,7 @@ Page({
     // 左侧的菜单数据
     leftMenuList: ['1', '2'],
     // 右侧的商品数据
-    rightContent: [
-      {
-        id: 1,
-        mname: "分类1",
-        children: [
-          { id: 11, sname: "子分类1-1" },
-          { id: 12, sname: "子分类1-2" },
-          // 其他子分类项
-        ]
-      },
-      {
-        id: 2,
-        mname: "分类2",
-        children: [
-          { id: 21, sname: "子分类2-1" },
-          { id: 22, sname: "子分类2-2" },
-          // 其他子分类项
-        ]
-      }
-    ],
+    rightContent: [],
     // 被点击的左侧菜单
     currentIndex: 0,
     currentIndexJob: -1,
@@ -52,23 +33,30 @@ Page({
   onLoad: function (options) {
     this.getCates2();
   },
-  onShow: function () { },
-
-  onNavigatorTap: function () {
-    console.log('点击了 navigator 元素！');
-  },
   //获取分类数据
   async getCates2() {
 
-    const resultRes = await db.collection('category').doc("80e3bed06567362301b9fea3075992be").get()
-    const result = resultRes.data.industry
+    // const resultRes = await db.collection('category').doc("80e3bed06567362301b9fea3075992be").get()
+    // const result = resultRes.data.industry
+    // this.setData({
+    //   Cates2: result
+    // })
+    // let leftMenuList = result.map(v => v.name);
+    // let rightContent = result[0].child;
+    // this.setData({
+    //   leftMenuList,
+    //   rightContent
+    // })
+
+    const resultRes = await db.collection('category').doc("aa14493d6567f7db008144531e794ebd").get()
+    const result = resultRes.data.occupation
     // console.log(result[0])
     this.setData({
       Cates2: result
     })
     let leftMenuList = result.map(v => v.name);
     // console.log(leftMenuList)
-    let rightContent = result[0].child;
+    let rightContent = result[0].subList;
     this.setData({
       // expectJobs,
       leftMenuList,
@@ -83,34 +71,15 @@ Page({
     1 获取被点击的标题身上的索引
     2 给data中的currentIndex赋值就可以了
     */
-    const { index } = e.currentTarget.dataset;
+    // const { index } = e.currentTarget.dataset;
+    const { index } = e.detail;
 
-    let rightContent = this.data.Cates2[index].child;
+    // let rightContent = this.data.Cates2[index].child;
+    let rightContent = this.data.Cates2[index].subList;
     this.setData({
       currentIndex: index,
       rightContent,
-      // 重新设置 他右侧内容的scroll-view标签的距离顶部的距离
       scrollTop: 0
     })
-    // let rightContent = [
-    //   {
-    //     id: 3,
-    //     mname: "分类5",
-    //     children: [
-    //       { id: 11, sname: "子分类1-1" },
-    //       { id: 12, sname: "子分类1-2" },
-    //       // 其他子分类项
-    //     ]
-    //   },
-    //   {
-    //     id: 4,
-    //     mname: "分类6",
-    //     children: [
-    //       { id: 21, sname: "子分类2-1" },
-    //       { id: 22, sname: "子分类2-2" },
-    //       // 其他子分类项
-    //     ]
-    //   }
-    // ]
   }
 })
